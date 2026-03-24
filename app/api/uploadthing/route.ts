665cbc05299603d83a0782ab131f2f643eb736ea
kerlos fouad <kokomx10@gmail.com>
2026-03-24 21:24:30 +0200

    fix: remove empty duplicate api folder

--- app/api/uploadthing/route.ts
+++
@@ -1,7 +0,0 @@
-import { createRouteHandler } from "uploadthing/next";
-import { ourFileRouter } from "./core";
-
-// Next.js هيسحب الـ Token تلقائياً من الـ .env
-export const { GET, POST } = createRouteHandler({
-  router: ourFileRouter,
-});
